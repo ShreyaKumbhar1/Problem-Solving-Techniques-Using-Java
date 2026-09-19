46c965bfcd64c560a1212039ad2933a8bf600645
shreyakumbhar28 <vtu30404@veltech.edu.in>
2026-09-19 21:32:03 +0530

    Unit 1 Week 1 Task 8 - Squares of a Sorted Array

---
+++ src/pstj_Week1/SquaresOfaSortedArray_8.java
@@ -0,0 +1,46 @@
+package pstj_Week1;
+
+import java.util.Scanner;
+
+public class SquaresOfaSortedArray_8 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		int[] nums = new int[n];
+		int[] result = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			nums[i] = sc.nextInt();
+		}
+
+		int left = 0;
+		int right = n - 1;
+		int index = n - 1;
+
+		while (left <= right) {
+
+			int leftSquare = nums[left] * nums[left];
+			int rightSquare = nums[right] * nums[right];
+
+			if (leftSquare > rightSquare) {
+				result[index] = leftSquare;
+				left++;
+			} else {
+				result[index] = rightSquare;
+				right--;
+			}
+
+			index--;
+		}
+
+		for (int i = 0; i < n; i++) {
+			System.out.print(result[i] + " ");
+		}
+
+		sc.close();
+	}
+}
