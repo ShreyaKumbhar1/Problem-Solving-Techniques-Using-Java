b835341e4cc05b80d4f139ee9ca9ebedf8b17dc5
shreyakumbhar28 <vtu30404@veltech.edu.in>
2026-09-22 21:28:46 +0530

    Unit 1 Week 2 Task 4 - Remove Duplicates From Sorted Array

---
+++ src/pstj_Week2/RemoveDuplicatesFromSortedArray_4.java
@@ -0,0 +1,42 @@
+package pstj_Week2;
+
+import java.util.Scanner;
+
+public class RemoveDuplicatesFromSortedArray_4 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		int[] nums = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			nums[i] = sc.nextInt();
+		}
+
+		if (n == 0) {
+			System.out.println(0);
+			sc.close();
+			return;
+		}
+
+		int k = 1;
+
+		for (int i = 1; i < n; i++) {
+			if (nums[i] != nums[k - 1]) {
+				nums[k] = nums[i];
+				k++;
+			}
+		}
+
+		System.out.println(k);
+
+		for (int i = 0; i < k; i++) {
+			System.out.print(nums[i] + " ");
+		}
+
+		sc.close();
+	}
+}
