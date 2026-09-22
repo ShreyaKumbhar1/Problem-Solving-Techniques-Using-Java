9f135fea612bdd002a0f11d9b3f34f46d7b46c18
shreyakumbhar28 <vtu30404@veltech.edu.in>
2026-09-22 21:27:49 +0530

    Unit 1 Week 2 Task 3 - Remove Element

---
+++ src/pstj_Week2/RemoveElement_3.java
@@ -0,0 +1,37 @@
+package pstj_Week2;
+
+import java.util.Scanner;
+
+public class RemoveElement_3 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		int[] nums = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			nums[i] = sc.nextInt();
+		}
+
+		int val = sc.nextInt();
+		int k = 0;
+
+		for (int i = 0; i < n; i++) {
+			if (nums[i] != val) {
+				nums[k] = nums[i];
+				k++;
+			}
+		}
+
+		System.out.println(k);
+
+		for (int i = 0; i < k; i++) {
+			System.out.print(nums[i] + " ");
+		}
+
+		sc.close();
+	}
+}
